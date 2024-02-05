9828c35efe68a56c43025cb118dfbe821ae6b0d3
RussBaz <RussBaz@users.noreply.github.com>
2024-02-05 17:58:16 +0600

    Added basic swift file generation

--- Sources/ReparseHtml.swift
+++ Sources/ReparseHtml.swift
@@ -1,84 +1,87 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-// 
+//
 // Swift Argument Parser
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
 import ArgumentParser
 import Foundation
 
+struct PageDef {
+    let path: String
+    let name: [String]
+}
+
 @main
 struct ReparseHtml: ParsableCommand {
     @Argument(help: "The target data folder location.", transform: URL.init(fileURLWithPath:))
     var location: URL
-    
+
     mutating func run() throws {
         guard directoryExists(at: location.path) else {
             throw ValidationError("Folder does not exist at \(location.path)")
         }
-        
+
         let htmls = findAllFiles(in: [location.path])
-        
-        print("Files:")
-        
-        for path in htmls {
-            print(path)
-        }
+
+        let ast = AST.from(htmls)
+
+        print(ast.build())
     }
-    
+
     func directoryExists(at path: String) -> Bool {
         var isDirectory: ObjCBool = false
         let exists = FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory)
         return exists && isDirectory.boolValue
     }
-    
-    func findAllFiles(in directories: [String]) -> [(String, [String])] {
-        var result: [(String, [String])] = []
-        
+
+    func findAllFiles(in directories: [String]) -> [PageDef] {
+        var result: [PageDef] = []
+
         for path in directories {
             let foundFiles = findFilesInDirectory(at: path)
             result.append(contentsOf: foundFiles)
         }
-        
+
         return result
     }
-    
-    func findFilesInDirectory(at path: String) -> [(String, [String])] {
+
+    func findFilesInDirectory(at path: String) -> [PageDef] {
         guard directoryExists(at: path) else { return [] }
-        
+
         guard let enumerator = FileManager.default.enumerator(atPath: path) else {
             return []
         }
         let paths = enumerator.allObjects as! [String]
-        
-        var htmls: [(String, [String])] = []
-        
+
+        var htmls: [PageDef] = []
+
         for i in paths {
-            let url = URL.init(fileURLWithPath: "\(path)/\(i)")
+            let url = URL(fileURLWithPath: "\(path)/\(i)")
             let path = url.path
             if path.hasSuffix(".html") {
-                htmls.append((path, splitName(i)))
+                htmls.append(.init(path: path, name: splitName(i)))
             }
         }
-        
+
         return htmls
     }
-    
+
     func splitName(_ name: String) -> [String] {
         var r = name.split(separator: "/")
-        
+
         if r.last?.hasSuffix(".html") == true {
-            r[r.count-1] = r[r.count-1].dropLast(5)
+            r[r.count - 1] = r[r.count - 1].dropLast(5)
         }
-        
+
         if r.isEmpty {
             r = ["Index"]
         }
-        
+
         if r.first == "" {
             r[r.startIndex] = "Index"
         }
-        
-        return r.map(String.init)
+
+        return r.map(String.init).reversed()
     }
 }
